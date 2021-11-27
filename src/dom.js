cc401de9c915a539385530c1b8d8f295b90330f1
Phi Nguyen <phinguyen202@gmail.com>
2021-11-27 22:56:57 +0700

    fixing incorrect attribute assignment for HTMLElement

--- src/dom.js
+++ src/dom.js
@@ -1,20 +1,10 @@
 import { Component } from "./component.js";
 
-export function elt(type, ...rest) {
-    return build(document.createElement(type), ...rest);
-}
-
-const NS = 'http://www.w3.org/2000/svg';
-export function eltSVG(type, ...rest) {
-    return build(document.createElementNS(NS, type), ...rest);
-}
-
-function build(dom, props, ...children) {
-    for (const prop in props) {
-        dom.setAttribute(prop, props[prop])
-    }
+export function elt(type, props, ...children) {
+    const dom = document.createElement(type);
+    if (props) Object.assign(dom, props);
     for (const child of children) {
-        if (child instanceof HTMLElement || child instanceof SVGElement) {
+        if (child instanceof HTMLElement) {
             dom.appendChild(child);
         } else if (typeof child === 'string') {
             dom.appendChild(document.createTextNode(child));
@@ -23,6 +13,28 @@ function build(dom, props, ...children) {
             dom.appendChild(child.element);
         }
     }
+    return dom;
+}
+
+const NS = 'http://www.w3.org/2000/svg';
+export function eltSVG(type, props, ...children) {
+    const dom = document.createElementNS(NS, type);
+    if (props) {
+        for (const prop in props) {
+            dom.setAttribute(prop, props[prop])
+        }
+    }
+    for (const child of children) {
+        if (child instanceof SVGElement) {
+            dom.appendChild(child);
+        } else if (typeof child === 'string') {
+            dom.appendChild(document.createTextNode(child));
+        } else if (child instanceof Component) {
+            child.element = child.render();
+            dom.appendChild(child.element);
+        }
+    }
+    return dom;
 }
 
 export function eltRef(parent, name, component) {
